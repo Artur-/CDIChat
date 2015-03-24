d7bb10e9e65d18f6684e921c84478b868e3d69a2
Artur Signell <artur@vaadin.com>
2015-03-24 19:43:33 +0200

    Udpate to 7.4 and built-in declarative

--- src/main/java/org/vaadin/artur/cdichat/CDIChat.java
+++ src/main/java/org/vaadin/artur/cdichat/CDIChat.java
@@ -1,6 +1,5 @@
 package org.vaadin.artur.cdichat;
 
-import java.io.InputStream;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.logging.Level;
@@ -13,122 +12,112 @@ import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.TextMessage;
 
-import org.vaadin.teemu.clara.Clara;
-import org.vaadin.teemu.clara.binder.annotation.UiField;
-import org.vaadin.teemu.clara.binder.annotation.UiHandler;
-
 import com.vaadin.annotations.Push;
 import com.vaadin.annotations.Theme;
 import com.vaadin.cdi.CDIUI;
-import com.vaadin.data.Property;
+import com.vaadin.data.Property.ValueChangeEvent;
+import com.vaadin.data.Property.ValueChangeListener;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.WebBrowser;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.Component;
-import com.vaadin.ui.ComponentContainer;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Notification;
-import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.UIDetachedException;
-import com.vaadin.ui.VerticalLayout;
 
 @CDIUI("")
 @Push
 @Theme("valo")
 public class CDIChat extends UI implements MessageListener {
 
-    @UiField
-    private VerticalLayout chatLog;
+	@Inject
+	private JMSMessenger jms;
+	private CDIChatLayout layout;
 
-    @Inject
-    private JMSMessenger jms;
+	@PostConstruct
+	public void registerConsumer() {
+		try {
+			jms.setMessageListener(this);
+		} catch (JMSException e) {
+			Logger.getLogger(getClass().getName()).log(Level.WARNING,
+					"Unable to register consumer", e);
+		}
+	}
 
-    @PostConstruct
-    public void registerConsumer() {
-        try {
-            jms.setMessageListener(this);
-        } catch (JMSException e) {
-            Logger.getLogger(getClass().getName()).log(Level.WARNING,
-                    "Unable to register consumer", e);
-        }
-    }
+	@Override
+	protected void init(VaadinRequest request) {
+		layout = new CDIChatLayout();
+		setContent(layout);
+		layout.chatMessage.addValueChangeListener(new ValueChangeListener() {
 
-    @Override
-    protected void init(VaadinRequest request) {
-        InputStream res = getClass().getResourceAsStream("CDIChat.xml");
-        Component content = (Clara.create(res, this));
-        setContent(content);
-    }
+			@Override
+			public void valueChange(ValueChangeEvent event) {
+				String message = layout.chatMessage.getValue();
+				layout.chatMessage.setValue("");
+				sendMessage(message);
+			}
+		});
+	}
 
-    /**
-     * Called by Clara whenever the text input changes
-     */
-    @UiHandler("chatMessage")
-    public void onTextInput(Property.ValueChangeEvent event) {
-        TextField field = (TextField) event.getProperty();
-        if (!"".equals(field.getValue())) {
-            String text = field.getValue();
-            try {
-                jms.sendMessage(getUserId() + ": " + text);
-                field.setValue("");
-            } catch (JMSException e) {
-                Notification.show("Unable to send message. See server log");
-                Logger.getLogger(getClass().getName()).log(Level.WARNING,
-                        "Unable to send message", e);
-            }
-        }
-    }
+	public void sendMessage(String message) {
+		if (!"".equals(message)) {
+			try {
+				jms.sendMessage(getUserId() + ": " + message);
+			} catch (JMSException e) {
+				Notification.show("Unable to send message. See server log");
+				Logger.getLogger(getClass().getName()).log(Level.WARNING,
+						"Unable to send message", e);
+			}
+		}
+	}
 
-    /**
-     * Just return something which uniquely identifies the user
-     */
-    private String getUserId() {
-        WebBrowser browser = getSession().getBrowser();
-        String browserString = browser.isChrome() ? "Chrome" : browser
-                .isFirefox() ? "Firefox" : browser.isIE() ? "IE" : browser
-                .isSafari() ? "Safari" : "Unknown";
-        browserString += " " + browser.getBrowserMajorVersion();
-        String osString = browser.isWindows() ? "Windows"
-                : browser.isMacOSX() ? "Mac" : browser.isLinux() ? "Linux"
-                        : "Unknown";
-        return hashCode() + " (" + browserString + "/" + osString + ")";
-    }
+	/**
+	 * Just return something which uniquely identifies the user
+	 */
+	private String getUserId() {
+		WebBrowser browser = getPage().getWebBrowser();
+		String browserString = browser.isChrome() ? "Chrome" : browser
+				.isFirefox() ? "Firefox" : browser.isIE() ? "IE" : browser
+				.isSafari() ? "Safari" : "Unknown";
+		browserString += " " + browser.getBrowserMajorVersion();
+		String osString = browser.isWindows() ? "Windows"
+				: browser.isMacOSX() ? "Mac" : browser.isLinux() ? "Linux"
+						: "Unknown";
+		return hashCode() + " (" + browserString + "/" + osString + ")";
+	}
 
-    /**
-     * Called by the MessageConsumer whenever a JMS message arrives
-     */
-    @Override
-    public void onMessage(Message message) {
-        TextMessage textMessage = (TextMessage) message;
-        final String text;
-        try {
-            text = textMessage.getText();
-            System.out.println("incoming message " + text + " for " + getUIId()
-                    + "(" + this + ")");
-            try {
+	/**
+	 * Called by the MessageConsumer whenever a JMS message arrives
+	 */
+	@Override
+	public void onMessage(Message message) {
+		TextMessage textMessage = (TextMessage) message;
+		final String text;
+		try {
+			text = textMessage.getText();
+			System.out.println("incoming message " + text + " for " + getUIId()
+					+ "(" + this + ")");
+			try {
 
-                access(new Runnable() {
-                    @Override
-                    public void run() {
-                        DateFormat df = DateFormat
-                                .getTimeInstance(DateFormat.MEDIUM);
-                        Label l = new Label(df.format(new Date()) + ": " + text);
-                        l.setSizeUndefined();
-                        chatLog.addComponent(l);
-                    }
-                });
-            } catch (UIDetachedException e) {
-                Logger.getLogger(getClass().getName()).info(
-                        "Trying to send message to detached UI");
-            }
-        } catch (JMSException e) {
-            Notification.show("Unable to retrieve message. See server log");
-            Logger.getLogger(getClass().getName()).log(Level.WARNING,
-                    "Unable to retrieve message", e);
-        }
+				access(new Runnable() {
+					@Override
+					public void run() {
+						DateFormat df = DateFormat
+								.getTimeInstance(DateFormat.MEDIUM);
+						Label l = new Label(df.format(new Date()) + ": " + text);
+						l.setSizeUndefined();
+						layout.chatLog.addComponent(l);
+					}
+				});
+			} catch (UIDetachedException e) {
+				Logger.getLogger(getClass().getName()).info(
+						"Trying to send message to detached UI");
+			}
+		} catch (JMSException e) {
+			Notification.show("Unable to retrieve message. See server log");
+			Logger.getLogger(getClass().getName()).log(Level.WARNING,
+					"Unable to retrieve message", e);
+		}
 
-    }
+	}
 
 }
